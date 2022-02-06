e33b5dd9e09e4ea7b60db183b21785f3f5be8dd5
Cory Starks <coryd.starks@gmail.com>
2022-02-05 22:11:24 -0800

    Made more progress on Scanning a directory

--- Programming assignment 1/src/DatabaseInitializer.java
+++ Programming assignment 1/src/DatabaseInitializer.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 public class DatabaseInitializer extends DatabaseTool{
     //Database intiializer is going to initialize a database that has some information.
     //Information that is stored in a parent class
-    public void DatabaseInitializer () {
+    public void init () {
          DirectoryScan init =  new DirectoryScan();
+         init.addNewDirectory();
 
-         init.addNewDirectory("Directory" + 1);
-        //TODO: Step 1: Create Database Folder if one does not exist
-        //get current directory
-
-        //TODO: Step 2: inside of the Database Folder, Create a table folder
-
-        //TODO: Step 3: POSSIBLY create a config file if there is time.
     }
 }
+//TODO: Step 1: Create Database Folder if one does not exist
+//get current directory
+
+//TODO: Step 2: inside of the Database Folder, Create a table folder whether thats default table or specified one
+
+//TODO: Step 3: POSSIBLY create a config file if there is time.
--- Programming assignment 1/src/DirectoryScan.java
+++ Programming assignment 1/src/DirectoryScan.java
@@ -7,34 +7,46 @@ public class DirectoryScan{
     private String workingDirectory;
     private Scanner scan;
     private Integer folderNumberIterator;
+    private Integer fileNumberIterator;
+    private static File folder;
+    private static File[] listOfFiles;
+    private static Set<String> setOfDirectoryNames;
+    private static Set<String> setOfFileNames;
+
     public DirectoryScan(){
         this.scan = new Scanner(System.in);
         this.workingDirectory = System.getProperty("user.dir");
-        this.folderNumberIterator = 0;
+        setOfDirectoryNames =  new HashSet<>();
+        setOfFileNames = new HashSet<>();
+        folder = new File(workingDirectory);
+        listOfFiles = folder.listFiles();
+        for (File itFile : listOfFiles) {
+            for (int i = 0; i < listOfFiles.length; i++) {
+                if (listOfFiles[i].isFile()) {
+                    if (listOfFiles[i].getName().contains("_Table")){
+                        fileNumberIterator++;
+                    }
+                    setOfFileNames.add(listOfFiles[i].getName());
+                } else if (listOfFiles[i].isDirectory()) {
+                    if (listOfFiles[i].getName().contains("_Database")){
+                        folderNumberIterator++;
+                    }
+                    setOfDirectoryNames.add(listOfFiles[i].getName());
+                }
+            }
+        }
+        //TODO: error catching
     }
 
     public String scanDirectory(){ return this.scan.next(); }
 
-    public boolean addNewDirectory(String directoryName) {
-        File file = new File(workingDirectory + directoryName);
-        return file.mkdir();
+    public boolean addNewDirectory() {
+        //TODO:add a default directory
+        return false;
     }
 
     public boolean doesThisDirectoryExist(String thisDirectory){
-        Set<String> setOfDirectoryNames = new HashSet<>();
-        Set<String> setOfFileNames = new HashSet<>();
-        Integer tmpInteger = folderNumberIterator;
-        File folder = new File(workingDirectory);
-
-        //iterate over directory names
-        File[] listOfFiles = folder.listFiles();
-        for (int i = 0; i < listOfFiles.length; i++) {
-            if (listOfFiles[i].isFile()) {
-                //System.out.println("File " + listOfFiles[i].getName());
-            } else if (listOfFiles[i].isDirectory()) {
-                setOfDirectoryNames.add(listOfFiles[i].getName());
-            }
-        }
+        //TODO: error catching
         for (String d : setOfDirectoryNames) {
             if (thisDirectory.equals(d)) {
                 return false;
